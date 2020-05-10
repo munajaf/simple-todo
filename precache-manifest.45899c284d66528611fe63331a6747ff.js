180f64155a78e54435fcd8bf251c993feb40b17c
munajaf <munajaf@users.noreply.github.com>
2020-05-10 12:08:37 +0000

    test more deployment 5bc84fbf06c05bc99f7166dc489265cd4657c90a

---
+++ precache-manifest.45899c284d66528611fe63331a6747ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f0ced5db7f48061f3e1808d512d72d",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "4c937b6192193426843e",
+    "url": "/simple-todo/static/css/main.a6de30eb.chunk.css"
+  },
+  {
+    "revision": "304da1d1fe2f1f6a4d1e",
+    "url": "/simple-todo/static/js/2.e44561d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-todo/static/js/2.e44561d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c937b6192193426843e",
+    "url": "/simple-todo/static/js/main.a1696654.chunk.js"
+  },
+  {
+    "revision": "98d3e45b4c7abec8439c",
+    "url": "/simple-todo/static/js/runtime-main.a53ccdbd.js"
+  }
+]);
